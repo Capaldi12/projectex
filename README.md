a3dde5f852547b197d7efc16c40f278d9bab6dc2
Capaldi <4sentepoid4@gmail.com>
2021-08-01 01:08:04 +0300

    Cleaned up mods.toml file Added logo Tried to add creative tab Made todo list (kinda) Added original mod textures

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-1.16 port of LatvianModder's Project EX mod
+#ProjectEX
+
+This is port of Project EX by LatvianModder
+I have not created any part of this mod, only adapted it to work with minecraft 1.16
+
+[Original mod curseforge page](https://www.curseforge.com/minecraft/mc-mods/project-ex)  
+[Original mod repository](https://github.com/LatvianModder/Project-EX)
+
+This mod is addon for [ProjectE](https://www.curseforge.com/minecraft/mc-mods/projecte) mod and requires it to work
+
+##Done
+
+- Created project
+
+##TODO
+
+- Items
+- Blocks
+- Tile Entities
+- Gui
+- Other
+
+
+order not enforced
+
+yeah, nice todo list, I hope nobody sees it.
--- src/main/java/net/capaldi12/mcmods/projectex/ProjectEX.java
+++ src/main/java/net/capaldi12/mcmods/projectex/ProjectEX.java
@@ -2,43 +2,69 @@ package net.capaldi12.mcmods.projectex;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("projectex")
+@Mod(ProjectEX.MOD_ID)
 public class ProjectEX
 {
+
+    public static final String MOD_ID = "projectex";
+
+    // Author also uses this in @Mod, but it seems it works differently now:
+    // the only thing @Mod accepts is `value` - mod id
+    public static final String MOD_NAME = "Project EX";
+    public static final String VERSION = "0.0.0.projectex";
+
     // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    static final Logger LOGGER = LogManager.getLogger();
+
+    // Creative tab - now Item Group
+    // Not appearing right now. I think I need to add items first
+    public static class ProjectEXGroup extends ItemGroup {
+        public ProjectEXGroup() {
+            super("ProjectEX");
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return RegistryObject.of(
+                    new ResourceLocation("projecte:klein_star_omega"),
+                    ForgeRegistries.ITEMS
+            ).get().getDefaultInstance();
+            // When I get to items
+            //return new ItemStack(ProjectEXItems.PERSONAL_LINK);
+        }
+    }
 
     public ProjectEX() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    @SubscribeEvent
     private void setup(final FMLCommonSetupEvent event)
     {
         // some preinit code
@@ -46,11 +72,20 @@ public class ProjectEX
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
+    @SubscribeEvent
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
+    //You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+/* Inter-Mod communications. Doesn't seem I need them
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
@@ -64,13 +99,11 @@ public class ProjectEX
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
+*/
 
+
+
+/* Class to contain RegistryEvents handlers. Can't I handle them in mod class itself?
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
@@ -81,4 +114,5 @@ public class ProjectEX
             LOGGER.info("HELLO from Register Block");
         }
     }
+*/
 }
